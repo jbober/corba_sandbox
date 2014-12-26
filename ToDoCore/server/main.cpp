dd094d356e05b22b7f9f4fd7aa866edcd8cdca1d
gerronimo <jerronimo@jerronimo-laptop.(none)>
2014-12-26 12:34:28 +0100

    adding server executable

---
+++ ToDoCore/server/main.cpp
@@ -0,0 +1,16 @@
+
+#include <time.h>
+#include <iostream>
+//#include "timeS.h"
+//class Time_impl : public virtual POA_Time {
+//public:
+//virtual TimeOfDay get_gmt() throw(CORBA::SystemException);
+//};
+//TimeOfDay
+//Time_impl::
+//get_gmt() throw(CORBA::SystemException)
+//{ 
+int main()
+{
+	return 0;
+}
